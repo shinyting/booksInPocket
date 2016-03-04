5f240a4a1e4e49dbba5f3e5ceff5cc4ed9fe0085
administator <administator>
2016-03-04 14:52:23 +0800

    modify list page

---
+++ public/javascripts/main.js
@@ -0,0 +1,12 @@
+$(function () {
+	$('#slider').slick({
+		dots: true,
+		speed: 500,
+		adaptiveHeight: true,
+		autoplay: true,
+		// centerMode: true,
+		// fade: true,
+		// prevArrow: '<a href="#" class="ui-btn ui-shadow ui-corner-all ui-icon-carat-l ui-btn-icon-notext"></a>',
+		// nextArrow: ''
+	})
+})
